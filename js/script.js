6640f70a23fd69d3fbf064712a01b790a212bedb
Marcelo-MV <marcelomventura@hotmail.com>
2022-08-29 15:15:43 -0300

    dados alt

--- js/script.js
+++ js/script.js
@@ -10,16 +10,22 @@ $(function() {
         success: function(xml) {
             $(xml).find('dog').each(function() {
                 let raca = $(this).find('raca').text();
+                let nome = $(this).find('nome').text();
                 let descricao = $(this).find('descricao').text();
                 let img = $(this).find('foto').text();
                 let video = $(this).find('video').text();
                 let racaTratada = raca.replace(/[^\w/\s]/gi,'').replace(/\s/g, '')
 
                 console.log (racaTratada)
-               if (img != ""){ $("#imagem").append(`
+               if (img != ""){ $("#cards").append(`
                <div class= "card">
-                <img class="imagem ${racaTratada}"src="${img}">
+                    <img class="imagem ${racaTratada}"src="${img}">
+                    <div class= "descricao">
+                        <h1>${nome}</h1>
+                        <p>${descricao}</p>
+                    </div>
                 </div>
+                
                 `)}
                 
             });
